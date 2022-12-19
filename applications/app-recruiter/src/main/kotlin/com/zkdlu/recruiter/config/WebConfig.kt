82015b5b0ec0ce4c6d9a4811a30e445805c8a35c
zkdlu <zkdlu159951@gmail.com>
2022-12-19 18:55:16 +0900

    cors 설정 추가

---
+++ applications/app-recruiter/src/main/kotlin/com/zkdlu/recruiter/config/WebConfig.kt
@@ -0,0 +1,16 @@
+package com.zkdlu.recruiter.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig: WebMvcConfigurer {
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST")
+            .maxAge(-1)
+    }
+}
