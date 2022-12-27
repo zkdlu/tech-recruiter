73af960467862c19dc536495aea593e5b5de32db
zkdlu <zkdlu159951@gmail.com>
2022-12-27 19:41:06 +0900

    feign decoder 위치 이동 (feign config)

--- applications/app-recruiter/src/main/kotlin/com/zkdlu/recruiter/job/toss/TossFeignConfig.kt
+++ applications/app-recruiter/src/main/kotlin/com/zkdlu/recruiter/job/toss/TossFeignConfig.kt
@@ -1,12 +1,16 @@
 package com.zkdlu.recruiter.job.toss
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import feign.Response
+import feign.codec.Decoder
 import org.springframework.beans.factory.ObjectFactory
 import org.springframework.beans.factory.ObjectProvider
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters
 import org.springframework.cloud.openfeign.support.HttpMessageConverterCustomizer
+import org.springframework.cloud.openfeign.support.ResponseEntityDecoder
 import org.springframework.cloud.openfeign.support.SpringDecoder
 import org.springframework.context.annotation.Bean
+import java.lang.reflect.Type
 
 class TossFeignConfig {
     @Bean
@@ -14,5 +18,19 @@ class TossFeignConfig {
                 messageConverterCustomizer: ObjectProvider<HttpMessageConverterCustomizer>,
                 objectMapper: ObjectMapper): TossFeignDecoder {
         return TossFeignDecoder(SpringDecoder(messageConverters, messageConverterCustomizer), objectMapper)
+    }
+}
+
+class TossFeignDecoder(
+    decoder: Decoder,
+    private val objectMapper: ObjectMapper,
+): ResponseEntityDecoder(decoder) {
+    override fun decode(response: Response?, type: Type?): Any {
+        return kotlin.runCatching {
+            objectMapper.readValue<List<TossJob>>(
+                response?.body()?.asInputStream(),
+                objectMapper.typeFactory.constructCollectionType(MutableList::class.java, TossJob::class.java)
+            )
+        }.getOrElse { listOf() }
     }
 }
